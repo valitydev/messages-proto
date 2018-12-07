1e040aedbca1de1af66025eb5fa407197b413021
n.pospolita <n.pospolita@rbkmoney.com>
2018-12-07 16:32:01 +0300

    review fix.

--- proto/messages.thrift
+++ proto/messages.thrift
@@ -8,21 +8,20 @@ typedef base.ID ConversationId
 
 
 struct User {
-    1: required UserId user_id // Какое у нас системное id? Стоит ли добавить роли?
-    2: optional string name // Можем ли мы определить наше имя в системе и имя пользователя?
+    1: required UserId user_id
 }
 
 struct Message {
     1: required MessageId message_id
     2: required string text
     3: required User author
-    4: required base.Timestamp timestamp // Надо ли? Чисто для сортировки. Мб в протоколе не надо.
+    4: required base.Timestamp timestamp
 }
 
 struct Conversation {
     1: required ConversationId conversation_id
     2: required list<Message> messages
-    3: required ConversationStatus status // Нужно ли в протоколе? Можно чисто для фильтрации оставить и внутри сервиса
+    3: required ConversationStatus status
 }
 
 enum ConversationStatus {
@@ -38,16 +37,11 @@ exception ConversationsNotFound {
     1: required list<ConversationId> ids
 }
 
-exception ConversationsAlreadyExist {
-    1: required list<ConversationId> ids
-}
-
 service MessageService {
 
     list<Conversation> GetConversations(1: list<ConversationId> conversation_ids, 2: ConversationFilter filter)
         throws (1: ConversationsNotFound ex)
 
     void SaveConversations(1: list<Conversation> conversations)
-        throws (1: ConversationsAlreadyExist ex)
 
 }
