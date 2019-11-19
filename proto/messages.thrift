391d225767fe8293a719dc22aab0580ad8715c72
vitaxa <vitaxa93gamebox@gmail.com>
2019-11-19 12:22:13 +0300

    remove user exception (#3)

--- proto/messages.thrift
+++ proto/messages.thrift
@@ -39,10 +39,6 @@ exception ConversationsNotFound {
     1: required list<ConversationId> ids
 }
 
-exception UsersNotProvided {
-    1: required list<UserId> ids
-}
-
 struct GetConversationResponse {
     1: required list<Conversation> conversations
     2: required map<UserId, User> users
@@ -53,7 +49,6 @@ service MessageService {
     GetConversationResponse GetConversations(1: list<ConversationId> conversation_ids, 2: ConversationFilter filter)
         throws (1: ConversationsNotFound ex)
 
-    void SaveConversations(1: list<Conversation> conversations, 2: list<User> users)
-        throws (1: UsersNotProvided ex)
+    void SaveConversations(1: list<Conversation> conversations, 2: User user)
 
 }
